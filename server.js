8dc9f67e057948c25f5baea4ce12ff871fa51860
theranjitkumar <web.ranjitkumar@gmail.com>
2019-12-09 20:38:09 -0500

    moment added

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
-    res.render('public/index.html');
+    res.render('./public/index.html');
 });
 
 app.listen(3000, '127.0.0.1', function(){
